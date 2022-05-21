d267bd2cfbff347ce59e611b387cf31f85bc1025
Inhoob <inhoob@gmail.com>
2022-05-21 19:55:38 +0900

    (leetcode) Queue : 232_ImplementQueueusingStacks

---
+++ 232_ImplementQueueusingStacks.py
@@ -0,0 +1,30 @@
+class MyQueue:
+    # stack 2개를 이용하여 큐 구현
+    # [2,3,1,4] 라고 생각
+    def __init__(self):
+        self.input = []
+        self.output = []
+
+    def push(self, x: int) -> None:
+        self.input.append(x)
+
+    def pop(self) -> int:
+        self.peek()
+        return self.output.pop()
+
+    def peek(self) -> int:
+        if not self.output:
+            while self.input:
+                self.output.append(self.input.pop())
+        return self.output[-1]  # 뒤집으면서 해결
+
+    def empty(self) -> bool:
+        return self.input == [] and self.output == []
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
