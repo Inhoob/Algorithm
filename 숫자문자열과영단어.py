72df6a248a8b298b57c941b6cf4e551970faa139
Inhoob <inhoob@gmail.com>
2022-05-26 11:02:08 +0900

    (programmers) complementation : 숫자문자열과영단어

--- 숫자문자열과영단어.py
+++ 숫자문자열과영단어.py
@@ -11,15 +11,19 @@ def solution(s):
         'eight': '8',
         'nine': '9'
     }
-    answer = ''
-    spelling = ''
-    for i in s:
-        if i.isdigit() == True:  # 일단 숫자는 다 뺀다.
-            answer += i
-            continue
-        spelling += i  # 스펠링을계속 더해준다. 예를들어 t,h,r,e,e 가 완성될때까지 계속 문자열을 더한 후
-        if spelling in words:  # 스펠링이 words의 키로 있으면 그 value 값을 출력해주고 spelling을 비워서 다음 문자를 받도록 함
-            answer += words[spelling]
-            spelling = ""
+    # 이 풀이는 몇몇 예시에서 시간 초과가 남
+    # answer = ''
+    # spelling = ''
+    # for i in s:
+    #     if i.isdigit() == True:  # 일단 숫자는 다 뺀다.
+    #         answer += i
+    #         continue
+    #     spelling += i  # 스펠링을계속 더해준다. 예를들어 t,h,r,e,e 가 완성될때까지 계속 문자열을 더한 후
+    #     if spelling in words:  # 스펠링이 words의 키로 있으면 그 value 값을 출력해주고 spelling을 비워서 다음 문자를 받도록 함
+    #         answer += words[spelling]
+    #         spelling = ""
+    answer = s
+    for item in words.items():
+        answer = answer.replace(item[0], item[1])
 
     return int(answer)
