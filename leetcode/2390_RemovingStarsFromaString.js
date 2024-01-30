6da528affd9112cf73a4c1da4a6e982112b0eb86
Inhoob <inhoob@gmail.com>
2024-01-30 22:59:43 +0900

    [leetcode][Stack][medium] : 2390_RemovingStarsFromaSTring

---
+++ leetcode/2390_RemovingStarsFromaString.js
@@ -0,0 +1,12 @@
+var removeStars = function (s) {
+  let stack = [];
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] !== "*") {
+      stack.push(s[i]);
+    } else {
+      stack.pop();
+    }
+  }
+
+  return stack.join("");
+};
